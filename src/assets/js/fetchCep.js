733af20efd86003feac6a114addfd82e4e6d6d58
Rafael Cerbaro <cerbarorafael1@gmail.com>
2024-11-05 20:02:22 -0300

    ajuste exibição de informações

--- src/assets/js/fetchCep.js
+++ src/assets/js/fetchCep.js
@@ -1,18 +1,20 @@
-const url = "https://viacep.com.br/ws/89874000/json/";
+const url = "https://viacep.com.br/ws/89801112/json/";
 
 export async function getCEPData(element) {
     let headers = new Headers();
     headers.append('Content-Type', 'application/json');
     try {
-        fetch(url,{
+        fetch(url, {
             headers: headers
         }).then(function (response) {
             // The API call was successful!
             return response.json();
         }).then(function (data) {
             // This is the JSON from our response
-            console.log(data);
-            element.innerHTML = `${data.cep} de ${data.localidade}/${data.uf}`
+            // console.log(data);
+            element.innerHTML = `${data.logradouro}, ${data.complemento} - ${data.bairro}
+            <br>${data.localidade}/${data.uf}, ${data.cep}
+            <br>${data.ddd} - ${data.regiao}`
         }).catch(function (err) {
             // There was an error
             console.warn('Something went wrong.', err);
--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,26 +1,15 @@
 import '../css/style.css'
-import javascriptLogo from '../img/javascript.svg'
-import viteLogo from '../img/vite.svg'
-import { setupCounter } from './counter.js'
-import { getCEPData } from './fetchCep.js'
+import { getCEPData} from "./fetchCEP.js";
 
 document.querySelector('#app').innerHTML = `
   <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
+    <h1>Qual seu CEP?</h1>
+    <div>
+        <input type="text" name="cep" id="cep_field">
+        <button type="submit" id="cep_get">GET</button>
     </div>
     <h2 id="cep">-</h2>
-    
   </div>
 `
-
-setupCounter(document.querySelector('#counter'))
 
 getCEPData(document.querySelector('#cep'))
